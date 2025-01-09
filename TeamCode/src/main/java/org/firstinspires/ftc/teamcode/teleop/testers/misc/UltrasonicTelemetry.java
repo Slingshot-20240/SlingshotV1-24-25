d0ad90532bf9ca8ef060046dfa70639fa0dd9c26
Aubrey Tipps <aatipps@gmail.com>
2025-01-09 10:06:14 -0600

    finalizing mechanism classes - ready for fsm!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/testers/misc/UltrasonicTelemetry.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/testers/misc/UltrasonicTelemetry.java
@@ -1,6 +1,10 @@
 package org.firstinspires.ftc.teamcode.teleop.testers.misc;
 
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
@@ -8,15 +12,19 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.teamcode.Robot;
 import org.firstinspires.ftc.teamcode.misc.gamepad.GamepadMapping;
 
+@TeleOp
+@Config
 public class UltrasonicTelemetry extends OpMode {
     private Robot robot;
     private GamepadMapping controls;
     double backTarget;
     double sideTarget;
+    private Telemetry telemetry;
     @Override
     public void init() {
         controls = new GamepadMapping(gamepad1, gamepad2);
         robot = new Robot(hardwareMap, telemetry, controls);
+        telemetry = new MultipleTelemetry(this.telemetry, FtcDashboard.getInstance().getTelemetry());
     }
 
     @Override
